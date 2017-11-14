2f8719d790d89dac96cc822965c87803dc6afa65
Ralpher <rafdimaso@gmail.com>
2017-11-14 14:19:14 +0100

    Finalizzato sistema di Grabbing (without Refactoring)

--- escape/Source/escape/Grabbatore.cpp
+++ escape/Source/escape/Grabbatore.cpp
@@ -2,6 +2,9 @@
 
 #include "Grabbatore.h"
 #include "Runtime/Engine/Public/DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "Components/PrimitiveComponent.h"
+#include "Components/InputComponent.h"
 
 
 #define OUTPUT
@@ -22,8 +25,37 @@ void UGrabbatore::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	//assegnazione dei componenti
+	FindPhysicsHandleComponent();
+	SetupInputComponent();
+
+}
+
+void UGrabbatore::SetupInputComponent()
+{
+	InputComp = GetOwner()->FindComponentByClass<UInputComponent>();
+
+	if (InputComp)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Got InputComponent"));
+		//Binding delle azioni 
+		InputComp->BindAction("Grab", IE_Pressed, this, &UGrabbatore::Grab);
+		InputComp->BindAction("Grab", IE_Released, this, &UGrabbatore::Release);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Error InputComponent not found"));
+	}
+}
+
+void UGrabbatore::FindPhysicsHandleComponent()
+{
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (!PhysicsHandle)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No PhysicsHandle"));
+	}
 }
 
 
@@ -31,32 +63,101 @@ void UGrabbatore::BeginPlay()
 void UGrabbatore::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
+	
 	FVector PlayerViewPortPosition;
 	FRotator PlayerViewPortRotator;
 
 	//definendo il MACRO OUT con #define, il metodo GetPlayerViewport è void ma get, funziona quindi così:
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
-	
+
 		OUT PlayerViewPortPosition,
 		OUT PlayerViewPortRotator
-	
+
 	);
 	// Log per aggiornare in ticking la position e rotation del Punto Vista del Giocatore.
 	/*UE_LOG(LogTemp, Warning, TEXT("Posizione: %s Rotazione: %s"),  *PlayerViewPortPosition.ToString(), *PlayerViewPortRotator.ToString());*/
 
+
+	FVector LineTraceEnd = PlayerViewPortPosition + PlayerViewPortRotator.Vector() * Reach;
+
+	if (PhysicsHandle->GrabbedComponent) 
+	{
+		PhysicsHandle->SetTargetLocation(LineTraceEnd);
+	}
+
 	
+}
+
+void UGrabbatore::Grab()
+{
+	UE_LOG(LogTemp, Log, TEXT("Grab key pressed"));
+	FHitResult Hit = GetFirstTraceBody();
+	auto ComponentGrabbato = Hit.GetComponent();
+	auto ActorHit = Hit.GetActor();
+
+	if (ActorHit)
+	{
+		PhysicsHandle->GrabComponentAtLocationWithRotation(
+			ComponentGrabbato,
+			NAME_None,
+			ComponentGrabbato->GetOwner()->GetActorLocation(),
+			ComponentGrabbato->GetOwner()->GetActorRotation()
+		);
+	}
+	else{ UE_LOG(LogTemp, Error, TEXT("Grabbing Error 104")); }
+}
+
+void UGrabbatore::Release()
+{
+	UE_LOG(LogTemp, Log, TEXT("Grab key released"));
+	PhysicsHandle->ReleaseComponent();
+}
+
+FHitResult UGrabbatore::GetFirstTraceBody() const
+{
+	FVector PlayerViewPortPosition;
+	FRotator PlayerViewPortRotator;
+
+	//definendo il MACRO OUT con #define, il metodo GetPlayerViewport è void ma get, funziona quindi così:
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+
+		OUT PlayerViewPortPosition,
+		OUT PlayerViewPortRotator
+
+	);
+	// Log per aggiornare in ticking la position e rotation del Punto Vista del Giocatore.
+	/*UE_LOG(LogTemp, Warning, TEXT("Posizione: %s Rotazione: %s"),  *PlayerViewPortPosition.ToString(), *PlayerViewPortRotator.ToString());*/
+
+
 	FVector LineTraceEnd = PlayerViewPortPosition + PlayerViewPortRotator.Vector() * Reach;
 
 	DrawDebugLine(
-		GetWorld(), 
-		PlayerViewPortPosition, 
+		GetWorld(),
+		PlayerViewPortPosition,
 		LineTraceEnd,
 		ColorLineDD,
-		false, 
-		0.f, 
+		false,
 		0.f,
-		5.0);
+		0.f,
+		10.0);
+	
+	FHitResult HitOut;
 
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT HitOut,
+		PlayerViewPortPosition,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		FCollisionQueryParams(FName(TEXT("")), false, GetOwner())
+	);
+
+	AActor* HittedActor = HitOut.GetActor();
+
+	if (HittedActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Actor colpito: %s"), *(HittedActor->GetName()));
+	}
+	return HitOut;
 }
 
