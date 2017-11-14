e6ceecacef8f6c4cdd9a4db6f1ae4160a6faf58b
Ralpher <rafdimaso@gmail.com>
2017-11-14 14:31:39 +0100

    Finalizzato sistema di Grabbing (without Refactoring)

--- escape/Source/escape/Grabbatore.h
+++ escape/Source/escape/Grabbatore.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabbatore.generated.h"
+
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -20,15 +22,28 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	// Setup Input Component	
+	void SetupInputComponent();
+
+	// Setup Physics Handle Component
+	void FindPhysicsHandleComponent();
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
 
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComp = nullptr;
+
 	float Reach = 100.f;
 
 	UPROPERTY(EditAnywhere)
 		FColor ColorLineDD;
 	
+	void Grab();
+	void Release();
+
+	FHitResult GetFirstTraceBody() const;
 };
